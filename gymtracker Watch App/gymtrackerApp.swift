a0d19b51c4ed586936882f2558136db834fa8e46
Olof Jonsson <olof.jonsson@me.com>
2023-07-29 08:53:41 +0200

    Initial Commit

---
+++ gymtracker Watch App/gymtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymtrackerApp.swift
+//  gymtracker Watch App
+//
+//  Created by Olof Jonsson on 2023-07-29.
+//
+
+import SwiftUI
+
+@main
+struct gymtracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
