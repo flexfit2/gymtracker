44aa224c431df7d2b4728a8e104d77cc4af917f9
Olof Jonsson <olof.jonsson@me.com>
2023-08-16 16:25:41 +0200

    vghj

--- gymtracker Watch App/View/ExerciseView.swift
+++ gymtracker Watch App/View/ExerciseView.swift
@@ -25,6 +25,9 @@ struct ExerciseView: View {
     @State private var isPrevWorkoutPresented: Bool = false
     @State var previousExc: ExerciseEnt = ExerciseEnt()
     @State var indForThisExercise: Int = 0
+    private static let topId = "topIdHere"
+
+
 
   
     func loadList() {
@@ -51,7 +54,7 @@ struct ExerciseView: View {
             print(previousExercises[indForThisExercise - 1].origin!.date!)
             print(previousExercises[indForThisExercise - 1].text!)
 //            previousExc = previousExercises[indForThisExercise - 1]
-//            
+//
 //            previousExc = previousExercises.dropLast().last ?? exerciseent
         }
         sets = exerciseent.exerciseSetArray
@@ -89,109 +92,113 @@ struct ExerciseView: View {
         //  .environment(\.managedObjectContext, self.moc)
         
         ScrollView {
-            VStack(alignment: .center, spacing: 3) {
-                HeaderView(title: nameString)
-                if exerciseent != previousExc {
-                    Image(systemName: "info.circle")
-                        .onTapGesture {
-                            isPrevWorkoutPresented.toggle()
-                        }
-                        .sheet(isPresented: $isPrevWorkoutPresented, content: {
-                            PrevWorkoutView(exercise: previousExc)
-                        })
-                }
-                Text(gympassDateText)
-                    .font(.system(size: 8, weight: .light))
-                Spacer()
-                HStack {
-                    DigiTextView(placeholder: "Reps",
-                                 text: $repsText,
-                                 presentingModal: presentingModal
-                    )
-                    .frame(width: 70)
-                    DigiTextView(placeholder: "Vikt",
-                                 text: $weightText,
-                                 presentingModal: presentingModal
-                    )
-                    Button {
-                        guard repsText.isEmpty == false else { return }
-                        print("Creating set")
-                        let exercisesetent = ExerciseSetEnt(context: moc)
-                        exercisesetent.id = UUID()
-                        exercisesetent.reps = Int32(repsText)!
-                        exercisesetent.weight = Float(weightText)!
-                        exercisesetent.name = String(Date().timeIntervalSince1970)
-                        // Link
-                        exercisesetent.exercise = exerciseent
-                        try? moc.save()
-                        //workaround for view to load
-                        sets = exerciseent.exerciseSetArray
-                        print("#########exerciseETDUMP###########")
-                        dump(exercisesetent)
-                        print("COUNT IS \(exerciseent.exerciseSetArray.count)")
-                        repsText = ""
-                        weightText = ""
-                    } label: {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 20, weight: .semibold))
+            ScrollViewReader { scrollProxy in
+                
+                VStack(alignment: .center, spacing: 3) {
+                    HeaderView(title: nameString).id(Self.topId)
+                    if exerciseent != previousExc {
+                        Image(systemName: "info.circle")
+                            .onTapGesture {
+                                isPrevWorkoutPresented.toggle()
+                            }
+                            .sheet(isPresented: $isPrevWorkoutPresented, content: {
+                                PrevWorkoutView(exercise: previousExc)
+                            })
                     }
-                }
-                Spacer()
-                if sets.count >= 1 {
-                    List {
-                        ForEach(0..<sets.count, id: \.self) { i in
-                            HStack {
+                    Text(gympassDateText)
+                        .font(.system(size: 8, weight: .light))
+                    Spacer()
+                    HStack {
+                        DigiTextView(placeholder: "Reps",
+                                     text: $repsText,
+                                     presentingModal: presentingModal
+                        )
+                        .frame(width: 70)
+                        DigiTextView(placeholder: "Vikt",
+                                     text: $weightText,
+                                     presentingModal: presentingModal
+                        )
+                        Button {
+                            guard repsText.isEmpty == false else { return }
+                            print("Creating set")
+                            let exercisesetent = ExerciseSetEnt(context: moc)
+                            exercisesetent.id = UUID()
+                            exercisesetent.reps = Int32(repsText)!
+                            exercisesetent.weight = Float(weightText)!
+                            exercisesetent.name = String(Date().timeIntervalSince1970)
+                            // Link
+                            exercisesetent.exercise = exerciseent
+                            try? moc.save()
+                            //workaround for view to load
+                            sets = exerciseent.exerciseSetArray
+                            print("#########exerciseETDUMP###########")
+                            dump(exercisesetent)
+                            print("COUNT IS \(exerciseent.exerciseSetArray.count)")
+                            repsText = ""
+                            weightText = ""
+                        } label: {
+                            Image(systemName: "plus.circle")
+                                .font(.system(size: 20, weight: .semibold))
+                        }
+                    }
+                    Spacer()
+                    if sets.count >= 1 {
+                        List {
+                            ForEach(0..<sets.count, id: \.self) { i in
                                 HStack {
-                                    Text("\(sets[i].reps) reps  ")
+                                    HStack {
+                                        Text("\(sets[i].reps) reps  ")
+                                            .lineLimit(1)
+                                    }
+                                    Capsule()
+                                        .frame(width: 4)
+                                        .foregroundColor(.accentColor)
+                                    Text("    \(sets[i].weight, specifier: "%.2f") kg")
                                         .lineLimit(1)
                                 }
-                                Capsule()
-                                    .frame(width: 4)
-                                    .foregroundColor(.accentColor)
-                                Text("    \(sets[i].weight, specifier: "%.2f") kg")
-                                    .lineLimit(1)
                             }
+                            .onDelete(perform: { offsets in deleteExerciseSetEnt(at: offsets)})
                         }
-                        .onDelete(perform: { offsets in deleteExerciseSetEnt(at: offsets)})
+                        .frame(height: 450)
+                        .listStyle(.carousel)
+                    } else {
+                        Spacer()
+                        Image(systemName: "note.text")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.gray)
+                            .opacity(0.25)
+                            .padding(25)
+                        Spacer()
                     }
-                    .frame(height: 450)
-                    .listStyle(.carousel)
-                } else {
-                    Spacer()
-                    Image(systemName: "note.text")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.gray)
-                        .opacity(0.25)
-                        .padding(25)
-                    Spacer()
                 }
-            }
-            .padding(3)
-            //            .onAppear(perform: {
-            //                loadList()
-            //            })
-            Picker(selection: $text,label: Text("")
-            ) {
-                Text("Byt namn")
-                    .tag(exerciseent.text)
-                    .foregroundColor(.cyan)
-                // .font(.system(size: 15, weight: .semibold))
-                ForEach(0..<exercisenames_sorted.count, id: \.self) { i in
-                    Text(exercisenames_sorted[i].text).font(.subheadline).tag("\(exercisenames_sorted[i].text)")
+                .padding(3)
+                //            .onAppear(perform: {
+                //                loadList()
+                //            })
+                Picker(selection: $text,label: Text("")
+                ) {
+                    Text("Byt namn")
+                        .tag(exerciseent.text)
+                        .foregroundColor(.cyan)
+                    // .font(.system(size: 15, weight: .semibold))
+                    ForEach(0..<exercisenames_sorted.count, id: \.self) { i in
+                        Text(exercisenames_sorted[i].text).font(.subheadline).tag("\(exercisenames_sorted[i].text)")
+                    }
                 }
-            }
-            .onChange(of: text) { print($0) }
-            .pickerStyle(.navigationLink)
-            .onAppear(perform: loadList)
-            Button {
-                print("changing name to \(text)")
-                exerciseent.text=text
-                nameString = exerciseent.text!
-                try? moc.save()
-            } label: {
-                Text("OK")
-                
+                .onChange(of: text) { print($0) }
+                .pickerStyle(.navigationLink)
+                .onAppear(perform: loadList)
+                Button {
+                    print("changing name to \(text)")
+                    exerciseent.text=text
+                    nameString = exerciseent.text!
+                    try? moc.save()
+                    scrollProxy.scrollTo(Self.topId, anchor: .top)
+                } label: {
+                    Text("OK")
+                    
+                }
             }
         }
     }
--- gymtracker Watch App/View/GymPassCoreView.swift
+++ gymtracker Watch App/View/GymPassCoreView.swift
@@ -18,6 +18,7 @@ struct GymPassCoreView: View {
     @FetchRequest(sortDescriptors: []) var exerciseArray: FetchedResults<ExerciseEnt>
     @State private var newDate = Date()
     let dateFormatter = DateFormatter()
+    private static let topId = "topIdHere"
     
     func loadList() {
         let tempArray = exercisenames
@@ -34,82 +35,79 @@ struct GymPassCoreView: View {
         }
         try? moc.save()
         sizeOfExerciseArray = gympassent.exerciseArray.count
-        
     }
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HeaderView(title: "Övningar")
-                Text(gympassent.text!)
-                    .font(.system(size: 8, weight: .light))
-                HStack {
-                    Picker(selection: $text,label: Text("")
-                    ) {
-                        if text == "Placeholder" {
-                            Text("Välj övning")
-                                .tag("Placeholder")
-                                .foregroundColor(.cyan)
-                                .font(.system(size: 25, weight: .semibold))
-                        }
-                        ForEach(0..<exercisenames_sorted.count, id: \.self) { i in
-                            Text(exercisenames_sorted[i].text).font(.subheadline).tag("\(exercisenames_sorted[i].text)")
-                        }
-                    }
-                    .onChange(of: text) { print($0) }
-                    .pickerStyle(.navigationLink)
-                    .onAppear().onAppear(perform: loadList)
-                    
-                    Button {
-                        guard text.isEmpty == false || text == "Placeholder" else { print("text is empty")
-                            return }
-                        // COREDATA STUFF
-                        let exerciseent = ExerciseEnt(context: moc)
-                        exerciseent.id = UUID()
-                        exerciseent.text = text
-                        exerciseent.origin = gympassent
-                        exerciseent.timestamp = String(Date().timeIntervalSince1970)
-                        try? moc.save()
-                        sizeOfExerciseArray = gympassent.exerciseArray.count
-                        //                        print("#########GYMSETDUMP###########")
-                        //                        dump(exerciseent)
-                        //                        print("#########GYMPASSDUMP###########")
-                        //                        dump(gympassent)
-                        //dump(gymsetents)
-                        text = "Placeholder"
-                    } label: {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 30, weight: .semibold))
-                            .padding(.top, 16)
-                    }
-                    //.buttonStyle(BorderedButtonStyle(tint: .white))
-                    //  .buttonStyle(PlainButtonStyle())
-                    .foregroundColor(Color.green)
-                }
-                Spacer()                
-                if sizeOfExerciseArray >= 1 {
-                    List {
-                        ForEach(gympassent.exerciseArray, id: \.self) { exerciseEnt in
-                            NavigationLink(destination: ExerciseView(exerciseent: exerciseEnt, gympassDateText: gympassent.text!)
-                                .environment(\.managedObjectContext, self.moc)
-                            ) {
-                                HStack {
-                                    Capsule()
-                                        .frame(width: 4, height: 20)
-                                        .foregroundColor(.accentColor)
-                                    Text(exerciseEnt.wrappedText)
-                                        .frame(height: 20)
-                                    Spacer()
-                                }
+            ScrollView {
+                ScrollViewReader { scrollProxy in
+
+                VStack {
+                    HeaderView(title: "Övningar").id(Self.topId)
+                    Text(gympassent.text!)
+                        .font(.system(size: 8, weight: .light))
+                    HStack {
+                        Picker(selection: $text,label: Text("")
+                        ) {
+                            if text == "Placeholder" {
+                                Text("Välj övning")
+                                    .tag("Placeholder")
+                                    .foregroundColor(.cyan)
+                                    .font(.system(size: 25, weight: .semibold))
+                            }
+                            ForEach(0..<exercisenames_sorted.count, id: \.self) { i in
+                                Text(exercisenames_sorted[i].text).font(.subheadline).tag("\(exercisenames_sorted[i].text)")
                             }
                         }
-                        .onDelete(perform: { offsets in deleteExerciseEnt(at: offsets)})
+                        .onChange(of: text) { print($0) }
+                        .pickerStyle(.navigationLink)
+                        .onAppear(perform: loadList)
+                        
+                        Button {
+                            guard text.isEmpty == false || text == "Placeholder" else { print("text is empty")
+                                return }
+                            // COREDATA STUFF
+                            let exerciseent = ExerciseEnt(context: moc)
+                            exerciseent.id = UUID()
+                            exerciseent.text = text
+                            exerciseent.origin = gympassent
+                            exerciseent.timestamp = String(Date().timeIntervalSince1970)
+                            try? moc.save()
+                            sizeOfExerciseArray = gympassent.exerciseArray.count
+                            text = "Placeholder"
+                        } label: {
+                            Image(systemName: "plus.circle")
+                                .font(.system(size: 30, weight: .semibold))
+                                .padding(.top, 16)
+                        }
+                        //.buttonStyle(BorderedButtonStyle(tint: .white))
+                        //  .buttonStyle(PlainButtonStyle())
+                        .foregroundColor(Color.green)
                     }
-                    .frame(height: 450)
-                    .listStyle(.carousel)
+                    Spacer()
 
-                }
+                    if sizeOfExerciseArray >= 1 {
 
+                        List {
+
+                            ForEach(gympassent.exerciseArray, id: \.self) { exerciseEnt in
+                                NavigationLink(destination: ExerciseView(exerciseent: exerciseEnt, gympassDateText: gympassent.text!)
+                                    .environment(\.managedObjectContext, self.moc)
+                                ) {
+                                    HStack {
+                                        Capsule()
+                                            .frame(width: 4, height: 20)
+                                            .foregroundColor(.accentColor)
+                                        Text(exerciseEnt.wrappedText)
+                                            .frame(height: 20)
+                                        Spacer()
+                                    }
+                                }
+                            }
+                            .onDelete(perform: { offsets in deleteExerciseEnt(at: offsets)})
+                        }
+                        .frame(height: 450)
+                        .listStyle(.carousel)
+                    }
                     else {
                         Spacer()
                         Image(systemName: "note.text")
@@ -120,30 +118,30 @@ struct GymPassCoreView: View {
                             .padding(25)
                         Spacer()
                     }
-              //  }
-                
+                }
+                //.fixedSize()
+                //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(.accentColor)
+                .onAppear(perform: {
+                    loadList()
+                })
+                DatePicker("Ändra datum", selection: $newDate)
+                Button {
+                    dateFormatter.dateStyle = DateFormatter.Style.medium
+                    dateFormatter.timeStyle = DateFormatter.Style.short
+                    print("changing name to \(text)")
+                    gympassent.date = newDate
+                    gympassent.text = "\(dateFormatter.string(from: newDate))"
+                    try? moc.save()
+                    scrollProxy.scrollTo(Self.topId, anchor: .top)
+                } label: {
+                    Text("OK")
+                    
+                }
             }
-            //.fixedSize()
-            //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
-            .buttonStyle(PlainButtonStyle())
-            .foregroundColor(.accentColor)
-            .onAppear(perform: {
-                loadList()
-            })
-            DatePicker("Ändra datum", selection: $newDate)
-            Button {
-                dateFormatter.dateStyle = DateFormatter.Style.medium
-                dateFormatter.timeStyle = DateFormatter.Style.short
-                print("changing name to \(text)")
-                gympassent.date = newDate
-                gympassent.text = "\(dateFormatter.string(from: newDate))"
-                try? moc.save()
-            } label: {
-                Text("OK")
-                
-            }
+            
         }
-        
     }
 }
 
